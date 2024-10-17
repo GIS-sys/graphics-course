d7d2a20db489c753f21b75e01670a3059920c32f
GISwarm <gordeiegorov@gmail.com>
2024-10-17 22:13:10 +0300

    copy local shadertoy 1

---
+++ tasks/local_shadertoy2/App.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <etna/Window.hpp>
+#include <etna/PerFrameCmdMgr.hpp>
+#include <etna/ComputePipeline.hpp>
+#include <etna/Image.hpp>
+#include <etna/GlobalContext.hpp>
+#include <etna/BlockingTransferHelper.hpp>
+#include <etna/Sampler.hpp>
+
+#include "wsi/OsWindowingManager.hpp"
+
+
+class App
+{
+public:
+  App();
+  ~App();
+
+  void run();
+
+private:
+  void drawFrame();
+
+private:
+  OsWindowingManager windowing;
+  std::unique_ptr<OsWindow> osWindow;
+
+  glm::uvec2 resolution;
+  bool useVsync;
+
+  std::unique_ptr<etna::Window> vkWindow;
+  std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
+
+  etna::ComputePipeline pipeline;
+  etna::Image toyMap;
+  std::unique_ptr<etna::BlockingTransferHelper> transferHelper;
+  etna::Sampler defaultSampler;
+};
