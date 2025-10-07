3d3d7061bd9eaf836e591fe93330be8b4c7536d4
GISwarm <gordeiegorov@gmail.com>
2025-10-07 19:11:42 +0300

    copy imgui

--- tasks/particles/App.cpp
+++ tasks/particles/App.cpp
@@ -11,77 +11,51 @@
 #include <chrono>
 #include <iostream>
 
+#include <imgui.h>
+#include "gui/ImGuiRenderer.hpp"
+
 
 App::App()
   : resolution{1280, 720}
   , useVsync{true}
 {
-  // First, we need to initialize Vulkan, which is not trivial because
-  // extensions are required for just about anything.
+  // Create window
+  osWindow = windowing.createWindow(OsWindow::CreateInfo{
+    .resolution = resolution,
+  });
+
+  // Init Vulkan
   {
-    // GLFW tells us which extensions it needs to present frames to the OS window.
-    // Actually rendering anything to a screen is optional in Vulkan, you can
-    // alternatively save rendered frames into files, send them over network, etc.
-    // Instance extensions do not depend on the actual GPU, only on the OS.
     auto glfwInstExts = windowing.getRequiredVulkanInstanceExtensions();
-
     std::vector<const char*> instanceExtensions{glfwInstExts.begin(), glfwInstExts.end()};
-
-    // We also need the swapchain device extension to get access to the OS
-    // window from inside of Vulkan on the GPU.
-    // Device extensions require HW support from the GPU.
-    // Generally, in Vulkan, we call the GPU a "device" and the CPU/OS combination a "host."
     std::vector<const char*> deviceExtensions{VK_KHR_SWAPCHAIN_EXTENSION_NAME};
-
-    // Etna does all of the Vulkan initialization heavy lifting.
-    // You can skip figuring out how it works for now.
     etna::initialize(etna::InitParams{
       .applicationName = "Local Shadertoy",
       .applicationVersion = VK_MAKE_VERSION(0, 1, 0),
       .instanceExtensions = instanceExtensions,
       .deviceExtensions = deviceExtensions,
-      // Replace with an index if etna detects your preferred GPU incorrectly
+      .features = vk::PhysicalDeviceFeatures2{.features = {}},
       .physicalDeviceIndexOverride = {},
       .numFramesInFlight = 1,
     });
   }
 
-  // Now we can create an OS window
-  osWindow = windowing.createWindow(OsWindow::CreateInfo{
-    .resolution = resolution,
-  });
-
-  // But we also need to hook the OS window up to Vulkan manually!
+  // Init window
   {
-    // First, we ask GLFW to provide a "surface" for the window,
-    // which is an opaque description of the area where we can actually render.
     auto surface = osWindow->createVkSurface(etna::get_context().getInstance());
-
-    // Then we pass it to Etna to do the complicated work for us
     vkWindow = etna::get_context().createWindow(etna::Window::CreateInfo{
       .surface = std::move(surface),
     });
-
-    // And finally ask Etna to create the actual swapchain so that we can
-    // get (different) images each frame to render stuff into.
-    // Here, we do not support window resizing, so we only need to call this once.
     auto [w, h] = vkWindow->recreateSwapchain(etna::Window::DesiredProperties{
       .resolution = {resolution.x, resolution.y},
       .vsync = useVsync,
     });
-
-    // Technically, Vulkan might fail to initialize a swapchain with the requested
-    // resolution and pick a different one. This, however, does not occur on platforms
-    // we support. Still, it's better to follow the "intended" path.
     resolution = {w, h};
+    guiRenderer = std::make_unique<ImGuiRenderer>(vkWindow->getCurrentFormat());
   }
 
-  // Next, we need a magical Etna helper to send commands to the GPU.
-  // How it is actually performed is not trivial, but we can skip this for now.
+  // Init shaders
   commandManager = etna::get_context().createPerFrameCmdMgr();
-
-
-  // TODO: Initialize any additional resources you require here!
   etna::create_program("local_shadertoy2_texture",
     {
       PARTICLES_SHADERS_ROOT "toy.vert.spv",
@@ -108,18 +82,15 @@ App::App()
       PARTICLES_SHADERS_ROOT "toy.frag.spv"
     }
   );
-
   pipeline = etna::get_context().getPipelineManager().createGraphicsPipeline(
     "shader",
     etna::GraphicsPipeline::CreateInfo{
       .fragmentShaderOutput =
         {
           .colorAttachmentFormats = {vkWindow->getCurrentFormat()}, //{vk::Format::eB8G8R8A8Srgb},
-          //.depthAttachmentFormat = vk::Format::eD32Sfloat,
         },
     }
   );
-
 
   int width;
   int height;
@@ -150,6 +121,9 @@ App::App()
     0, 0,
     std::span<const std::byte>(reinterpret_cast<const std::byte*>(loaded), width * height * channels)
   );
+
+  // Init ImGUI
+  ImGuiRenderer::enableImGuiForWindow(osWindow->native());
 }
 
 App::~App()
@@ -162,17 +136,50 @@ void App::run()
   while (!osWindow->isBeingClosed())
   {
     windowing.poll();
-
     drawFrame();
   }
 
-  // We need to wait for the GPU to execute the last frame before destroying
-  // all resources and closing the application.
   ETNA_CHECK_VK_RESULT(etna::get_context().getDevice().waitIdle());
 }
 
 void App::drawFrame()
 {
+  {
+    ZoneScopedN("drawGui");
+    guiRenderer->nextFrame();
+    ImGui::NewFrame();
+    ImGui::Begin("Simple render settings");
+
+    //float color[3]{uniformParams.baseColor.r, uniformParams.baseColor.g, uniformParams.baseColor.b};
+    //ImGui::ColorEdit3(
+    //  "Meshes base color", color, ImGuiColorEditFlags_PickerHueWheel | ImGuiColorEditFlags_NoInputs);
+    //uniformParams.baseColor = {color[0], color[1], color[2]};
+
+    //float pos[3]{uniformParams.lightPos.x, uniformParams.lightPos.y, uniformParams.lightPos.z};
+    //ImGui::SliderFloat3("Light source position", pos, -10.f, 10.f);
+    //uniformParams.lightPos = {pos[0], pos[1], pos[2]};
+
+    ImGui::Text(
+      "Application average %.3f ms/frame (%.1f FPS)",
+      1000.0f / ImGui::GetIO().Framerate,
+      ImGui::GetIO().Framerate);
+
+    ImGui::SliderInt("Objects Amount", &objectsAmount, 0, 256);
+
+    static const char* items[] {
+        "Nothing",
+        "Orbit",
+        "Look around",
+    };
+    ImGui::Combo("Mouse Control Type", &mouseControlType, items, IM_ARRAYSIZE(items));
+
+    ImGui::NewLine();
+
+    ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Press 'B' to recompile and reload shaders");
+    ImGui::End();
+    ImGui::Render();
+  }
+
   // First, get a command buffer to write GPU commands into.
   auto currentCmdBuf = commandManager->acquireNext();
 
@@ -223,6 +230,8 @@ void App::drawFrame()
         .res = res,
         .cursor = cursor,
         .time = (float)time,
+        .objectsAmount = objectsAmount,
+        .mouseControlType = mouseControlType,
       };
 
 
@@ -295,7 +304,11 @@ void App::drawFrame()
       currentCmdBuf.draw(3, 1, 0, 0);
       }
 
-
+      {
+        ImDrawData* pDrawData = ImGui::GetDrawData();
+        guiRenderer->render(
+          currentCmdBuf, {{0, 0}, {resolution.x, resolution.y}}, backbuffer, backbufferView, pDrawData);
+      }
 
       // At the end of "rendering", we are required to change how the pixels of the
       // swpchain image are laid out in memory to something that is appropriate
